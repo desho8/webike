b3d1d07b3db210face50bb49074882a9d919d50c
Dwesmond Ho <desho8@gmail.com>
2019-08-06 16:11:54 +0800

    modified BikesController

--- app/controllers/api/v1/bikes_controller.rb
+++ app/controllers/api/v1/bikes_controller.rb
@@ -1,30 +1,31 @@
 class Api::V1::BikesController < Api::V1::BaseController
-  skip_before_action: verify_authencity_token, only: [:create, :update, :destroy]
+  skip_before_action: verify_authencity_token, only: [:index, :update]
+  before_action :set_bike, only: [:index, :update]
 
   def index
     @bike = Bikes.all
     # render json: @bikes
   end
 
-  def show
-  end
+  # def show
+  # end
 
-  def create
-    # @bikes = Bike.create(bike_params)
-    # render json: @bike
-  end
+  # def create
+  #   # @bikes = Bike.create(bike_params)
+  #   # render json: @bike
+  # end
 
   def update
     @bike.update(bike_params)
     # render json: @bike
   end
 
-  def destroy
-    # @bike.destroy
-    # render json: @bike
-  end
+  # def destroy
+  #   # @bike.destroy
+  #   # render json: @bike
+  # end
 
-  # private
+  private
 
   def bike_params
     # params.require(:bike).permit(:text, :username)
@@ -33,5 +34,4 @@ class Api::V1::BikesController < Api::V1::BaseController
   def get_bike
     # @bike = Bike.find(params[:id])
   end
-
 end
