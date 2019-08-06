cc70816fae3d69ed0c12282cfe3d5ac33034cdf6
Yuman Xia <yumanxia@foxmail.com>
2019-08-06 15:54:17 +0800

    deleted double private

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,10 +14,7 @@ class Api::V1::UsersController < Api::v1::BaseController
     @user = User.find(params[:id])
   end
 
-  private
-
   def user_params
     params.require(:user).permit(:money)
   end
-
 end
