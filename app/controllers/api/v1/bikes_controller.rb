9012dcbfc1f4062fb4a90533541a95d2c91383ed
Andrey Maleev <andrey.maleyev@gmail.com>
2019-08-06 17:23:51 +0800

    removing errors

--- app/controllers/api/v1/bikes_controller.rb
+++ app/controllers/api/v1/bikes_controller.rb
@@ -1,29 +1,14 @@
 class Api::V1::BikesController < Api::V1::BaseController
-  skip_before_action: verify_authencity_token, only: [:index, :update]
+  skip_before_action verify_authencity_token, only: [:update]
   before_action :set_bike, only: [:index, :update]
 
   def index
     @bike = Bikes.all
-    # render json: @bikes
   end
-
-  # def show
-  # end
-
-  # def create
-  #   # @bikes = Bike.create(bike_params)
-  #   # render json: @bike
-  # end
 
   def update
     @bike.update(bike_params)
-    # render json: @bike
   end
-
-  # def destroy
-  #   # @bike.destroy
-  #   # render json: @bike
-  # end
 
   private
 
