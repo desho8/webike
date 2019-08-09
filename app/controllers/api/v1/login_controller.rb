2916ec86fbf948306c42e52ec0633eb5f0df00b8
Yuman Xia <yumanxia@foxmail.com>
2019-08-09 14:34:07 +0800

    fixed some bugs

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -4,21 +4,21 @@ class Api::V1::LoginController < Api::V1::BaseController
     URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
 
     def wechat_user
-      params = {
+      params_new = {
         appId: ENV['appId'],
         secret: ENV['secret'],
         js_code: params[:code],
-        grant_type: authorization_code
+        grant_type: "authorization_code"
       }
 
-      @wechat_response ||= RestClient.get(URL, params: params)
+      @wechat_response ||= RestClient.get(URL, params: params_new)
       @wechat_user ||= JSON.parse(@wechat_response.body)
     end
 
     def create
       @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
       render json: {
-        userId: @user.id
+        user: @user
       }
     end
   end
