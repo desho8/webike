d8b7782fba4c32c1ed1c7939b6217a7a7f6300ee
Yuman Xia <yumanxia@foxmail.com>
2019-08-09 13:59:21 +0800

    Add dotenv - Protect my secret data in .env file

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::LoginController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
     URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
-  
+
     def wechat_user
       params = {
         appId: appId,
@@ -8,11 +9,11 @@ class Api::V1::LoginController < Api::V1::BaseController
         js_code: params[:code],
         grant_type: authorization_code
       }
-  
+
       @wechat_response ||= RestClient.get(URL, params: params)
       @wechat_user ||= JSON.parse(@wechat_response.body)
     end
-  
+
     def create
       @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
       render json: {
